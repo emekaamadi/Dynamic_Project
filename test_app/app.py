2a0f86909bc5ca4db28fdf861fc7a252427ad146
Yunchae Shin <nightry@gmail.com>
2023-11-18 00:24:46 +0700

    commit

--- test_app/app.py
+++ test_app/app.py
@@ -1,51 +1,51 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from data_loader import load_and_filter_data
+import matplotlib.pyplot as plt
 from dynamic_pricing import DynamicPricing
-from utils import plot_cumulative_revenue, plot_demand_curve, plot_price_vs_cumulative_revenue
 
-# Streamlit 앱 타이틀 설정
-st.title('Dynamic Pricing Simulation for Lyft Rides')
 
-# 'Lyft'를 기반으로 데이터 로드 및 필터링
-df = load_and_filter_data('cab_rides.csv', 'Lyft')
+# Streamlit 앱의 제목 설정
+st.title('Dynamic Pricing Simulation \nfor Cab Rides')
 
-# 사용자 입력을 통한 product_id 및 source 선택
-product_id = st.selectbox('Select Lyft Service', df['product_id'].unique())
+# 데이터 로드
+df = pd.read_csv('cab_rides.csv')
+
+# 사용자 입력 처리
+product_id = st.selectbox('Select Service', df['product_id'].unique())
 source = st.selectbox('Select Source Location', df['source'].unique())
 
 # 사용자 입력에 기반한 데이터 필터링
 filtered_data = df[(df['product_id'] == product_id) & (df['source'] == source)]
 
-# 동적 가격 책정 시뮬레이션을 위한 사용자 입력
-initial_eta_mean = st.number_input('Initial η Mean', value=1.0)
-initial_eta_std = st.number_input('Initial η Standard Deviation', value=1.0)
-a = st.number_input('a Parameter', value=1.0)
-b = st.number_input('b Parameter', value=1.0)  # 'b' 매개변수 입력 추가
+# DynamicPricing 인스턴스 생성을 위한 매개변수 입력
+a = st.number_input('a Parameter', value=100.0)
+b = st.number_input('b Parameter', value=10.0)
 
-# DynamicPricing 객체 생성
-dynamic_pricing = DynamicPricing(initial_eta_mean, initial_eta_std, a, b)
+# DynamicPricing 인스턴스 생성 및 시뮬레이션
+if 'dynamic_pricing' not in st.session_state or st.button('Run Simulation'):
+    st.session_state.dynamic_pricing = DynamicPricing(data=filtered_data, a=a, b=b)
+    st.session_state.dynamic_pricing.simulate_pricing_with_revenue_tracking()
 
-# 시뮬레이션 실행 버튼
-if st.button('Run Simulation'):
-    dynamic_pricing.simulate_pricing_with_revenue_tracking(filtered_data)
+# 슬라이더 및 차트 표시
+if 'dynamic_pricing' in st.session_state:
+    # 슬라이더를 통한 데이터 포인트 선택
+    index_to_display = st.slider('Select Data Index', 0, len(st.session_state.dynamic_pricing.sampled_etas)-1, 0)
 
-    # 가격 범위 입력 받기
-    min_price = st.number_input('Minimum Price', value=0.0, step=0.1)
-    max_price = st.number_input('Maximum Price', value=10.0, step=0.1)
-    price_step = st.number_input('Price Step', value=0.1, step=0.1)
+    # 계산 중임을 나타내는 placeholder 생성
+    placeholder_demand_curve = st.empty()
+    placeholder_revenue_curve = st.empty()
 
-    # 시뮬레이션 버튼이 눌렸을 때 실행
-    if min_price < max_price and price_step > 0:
-        price_range = np.arange(min_price, max_price, price_step)
-        plot_price_vs_cumulative_revenue(dynamic_pricing, price_range)
-    else:
-        st.error("Please ensure that the max price is greater than the min price and the step is positive.")
-        
-    # 기존 수요 곡선 차트 유지
-    index = st.number_input('Select Index for Demand Curve', min_value=0, max_value=len(dynamic_pricing.actual_prices), step=1, value=0)
-    if index < len(dynamic_pricing.actual_prices):
-        plot_demand_curve(dynamic_pricing, index)
+    # 수요 곡선 차트 그리기
+    demand_curve_fig = st.session_state.dynamic_pricing.plot_demand_curve(index_to_display)
+    # 계산 중 메시지 표시
+    placeholder_demand_curve.text("Calculating the demand curve...")
+    # placeholder를 실제 수요 곡선 차트로 대체
+    placeholder_demand_curve.pyplot(demand_curve_fig)
 
-# 나머지 Streamlit 코드...
+    # 예상 매출 차트 그리기
+    price_range = np.linspace(0.01, 20, 200)  # 예상 매출 차트를 위한 가격 범위
+    revenue_fig, optimal_price, max_revenue = st.session_state.dynamic_pricing.plot_demand_and_revenue(price_range)
+    # placeholder를 실제 예상 매출 차트로 대체
+    placeholder_revenue_curve.pyplot(revenue_fig)
+    st.write(f"Optimal Price: {optimal_price:.2f}, Max Revenue: {max_revenue:.2f}")
--- test_app/dynamic_pricing.py
+++ test_app/dynamic_pricing.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
+import streamlit as st
+import pandas as pd
+import config as cfg
 
 class DynamicPricing:
-    def __init__(self, initial_eta_mean, initial_eta_std, a, b):
-        self.eta_mean = initial_eta_mean
-        self.eta_std = initial_eta_std
+    def __init__(self, data, a, b, eta_mean=None, eta_std=None):
+        self.data = data.copy()  # DataFrame의 복사본을 사용합니다.
+        self.eta_mean = eta_mean if eta_mean is not None else 0.5
+        self.eta_std = eta_std if eta_std is not None else 0.1
         self.a = a
         self.b = b
         self.price_points = []
@@ -14,92 +17,129 @@ class DynamicPricing:
         self.actual_prices = []
         self.revenue_history = []
         self.cumulative_revenue = 0
+        self.update_demand_observations()
 
-    def update_posterior(self, price, actual_price, demand):
-        if self.eta_std < 1e-6:
-            self.eta_std = 1e-6
-
-        eta_sample = -np.log(demand / self.a) / np.log(price)
-        likelihood_std = 0.1
-
-        new_mean_numerator = self.eta_mean / self.eta_std**2 + eta_sample / likelihood_std**2
-        new_mean_denominator = 1 / self.eta_std**2 + 1 / likelihood_std**2
-        new_mean = new_mean_numerator / new_mean_denominator if new_mean_denominator != 0 else self.eta_mean
-        new_std = (1 / self.eta_std**2 + 1 / likelihood_std**2)**(-0.5) if new_mean_denominator != 0 else self.eta_std
-
-        self.eta_mean = new_mean
-        self.eta_std = max(new_std, 1e-6)
+    def update_demand_observations(self):
+        # 데이터셋에서 수요 데이터를 처리하는 로직을 구현할 수 있습니다.
+        self.data['demand'] = self.data['distance'] * self.a  # 거리에 비례하는 수요로 가정
 
     def thompson_sampling(self):
+        # Thompson Sampling을 사용하여 eta 값을 샘플링합니다.
         sampled_eta = np.random.normal(self.eta_mean, self.eta_std)
         self.sampled_etas.append(sampled_eta)
         return sampled_eta
 
     def get_demand(self, price, eta):
+        # 주어진 가격과 eta 값에 따라 수요를 계산합니다.
         return self.a * price ** (-np.abs(eta))
+    # def get_demand(self, price, eta):
+    #     # 가격이 증가하면 수요가 감소하는 형태의 수요 함수
+    #     return self.a / (price ** np.abs(eta) + self.b)
 
-    def calculate_rmse(self):
-        estimated_prices = [self.get_price(d, e) for d, e in zip(self.actual_prices, self.sampled_etas)]
-        return np.sqrt(mean_squared_error(self.actual_prices, estimated_prices))
+    def simulate_pricing_with_revenue_tracking(self):
+        for index, row in self.data.iterrows():
+            sampled_eta = self.thompson_sampling()
+            demand = self.get_demand(row['price'], sampled_eta)
+            estimated_price = self.get_price(demand, sampled_eta)
+            revenue = row['price'] * demand
+            self.cumulative_revenue += revenue
+            self.revenue_history.append(self.cumulative_revenue)
+            self.actual_prices.append(row['price'])
+            self.eta_points.append(sampled_eta)
 
     def get_price(self, demand, eta):
+        # 주어진 수요와 eta 값에 따라 가격을 계산합니다.
         price = (demand / self.a) ** (-1 / np.abs(eta))
         self.price_points.append(price)
         return price
 
-    def calculate_revenue(self, price, demand):
-        return price * demand
-
-    def simulate_pricing_with_revenue_tracking(self, data):
-        for index, row in data.iterrows():
-            sampled_eta = self.thompson_sampling()
-            demand = self.get_demand(row['price'], sampled_eta)
-            estimated_price = self.get_price(demand, sampled_eta)
-            revenue = self.calculate_revenue(row['price'], demand)
-            self.cumulative_revenue += revenue
-            self.revenue_history.append(self.cumulative_revenue)
-
-            self.actual_prices.append(row['price'])
-            self.eta_points.append(sampled_eta)
-
-            self.update_posterior(row['price'], estimated_price, demand)
-
-            if index % 100 == 0 and index > 0:
-                print(f'Index: {index}, RMSE: {self.calculate_rmse()}, Cumulative Revenue: {self.cumulative_revenue}, Eta: {self.eta_mean:.4f}')
-
-    def simulate_pricing_with_revenue_estimation(self, price_range):
-        estimated_revenues = []
-        for price in price_range:
-            # 비선형 수요 함수 예시: a / (price + b) 형태로 모델링
-            # 여기서 a와 b는 모델 파라미터입니다.
-            demand_estimate = self.a / (price + self.b)
-            revenue_estimate = price * demand_estimate
-            estimated_revenues.append(revenue_estimate)
-
-        return price_range, estimated_revenues
-
     def plot_cumulative_revenue_over_time(self):
-        plt.figure(figsize=(10, 6))
-        plt.plot(self.revenue_history, label='Cumulative Revenue over time')
-        plt.xlabel('Time')
-        plt.ylabel('Cumulative Revenue')
-        plt.legend()
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(self.revenue_history, label='Cumulative Revenue over time')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Cumulative Revenue')
+        ax.legend()
+        ax.grid(True)
+        return fig  # fig 객체를 반환
+
+    def calculate_expected_revenue(self, price):
+        demand = self.get_demand(price, self.eta_mean)
+        return demand * price
+    
+    def calc_optimal_price(self, price_range):
+        # 가격 범위에 따른 예상 매출 계산
+        revenues = []
+        for price in price_range:
+            # 여기서 get_demand 함수는 실제 수요 데이터를 기반으로 한 추정치를 반환해야 합니다.
+            estimated_demand = self.get_demand(price, self.eta_mean)  # 현재 eta_mean을 사용하여 추정된 수요를 계산
+            revenue = price * estimated_demand  # 가격과 추정된 수요를 곱하여 매출을 계산
+            revenues.append(revenue)
+        
+        # 최대 매출과 해당하는 최적 가격 계산
+        max_revenue = max(revenues)
+        optimal_price_index = revenues.index(max_revenue)
+        optimal_price = price_range[optimal_price_index]
+        
+        return optimal_price, max_revenue
+    
+    def plot_demand_and_revenue(self, price_range):
+        # 가격에 따른 수요와 매출을 계산
+        demands = [self.get_demand(price, self.eta_mean) for price in price_range]
+        revenues = [price * demand for price, demand in zip(price_range, demands)]
+
+        # 최적 가격과 최대 매출 계산
+        optimal_price, max_revenue = self.calc_optimal_price(price_range)
+        
+        # 차트 그리기
+        fig, ax1 = plt.subplots(figsize=(10, 6))
+
+        # 수요 곡선 그리기
+        ax1.plot(price_range, demands, 'b-', label='Demand')
+        ax1.set_xlabel('Price')
+        ax1.set_ylabel('Demand', color='b')
+        ax1.tick_params('y', colors='b')
+
+        # 매출 곡선을 같은 그래프에 그리기 위한 두 번째 y축 추가
+        ax2 = ax1.twinx()
+        ax2.plot(price_range, revenues, 'r-', label='Revenue')
+        ax2.set_ylabel('Revenue', color='r')
+        ax2.tick_params('y', colors='r')
+
+        # 최적 가격 선 그리기
+        ax2.axvline(x=optimal_price, color='g', linestyle='--', label=f'Optimal Price: {optimal_price:.2f}')
+
+        # 범례 및 제목 설정
+        ax1.legend(loc='upper left')
+        ax2.legend(loc='upper right')
+        ax1.set_title('Demand and Revenue vs. Price')
+
         plt.grid(True)
         plt.show()
 
-    def plot_demand_curve(self, index):
-        if index < 100 or index >= len(self.sampled_etas):
-            print("Index out of range for plotting.")
-            return
-        plt.figure(figsize=(10, 6))
-        plt.scatter(self.price_points[:index], [self.get_demand(p, e) for p, e in zip(self.price_points[:index], self.eta_points[:index])], color='gray', s=10)
+        return fig, optimal_price, max_revenue
+    
+    def plot_demand_curve(self, index_to_display):
+        fig, ax = plt.subplots(figsize=(10, 6))
         price_range = np.linspace(0.5 * min(self.price_points), 1.5 * max(self.price_points), 100)
-        demand_curve = self.get_demand(price_range, self.sampled_etas[index-1])
-        plt.plot(price_range, demand_curve, label=f'η: {self.sampled_etas[index-1]:.4f}')
-        plt.xlabel('Price')
-        plt.ylabel('Demand')
-        plt.xlim([0, max(self.price_points)])
-        plt.ylim([0, max([self.get_demand(p, self.eta_mean) for p in price_range])])
-        plt.legend()
-        plt.grid(True)
-        plt.show()
+
+        # 이전 수요 곡선을 회색으로 투명하게 그리기
+        for past_index in range(100, index_to_display):
+            past_demand_curve = [self.get_demand(p, self.sampled_etas[past_index-1]) for p in price_range]
+            ax.plot(price_range, past_demand_curve, color='grey', alpha=0.1)  # 낮은 알파 값으로 거의 투명하게 표시
+
+        # 과거의 가격 데이터 포인트를 빨간색으로 누적하여 그리기
+        for past_index in range(100, index_to_display):
+            ax.scatter(self.price_points[past_index], self.get_demand(self.price_points[past_index], self.sampled_etas[past_index]), color='red', alpha=0.5)
+
+        # 현재 선택된 인덱스에 해당하는 수요 곡선을 진한 파란색으로 그리기
+        current_demand_curve = [self.get_demand(p, self.sampled_etas[index_to_display-1]) for p in price_range]
+        ax.plot(price_range, current_demand_curve, color='blue', alpha=1.0, label=f'η: {self.sampled_etas[index_to_display-1]:.4f}')  # 진한 파란색으로 표시
+
+        ax.set_xlabel('Price')
+        ax.set_ylabel('Demand')
+        ax.set_xlim([0, 17.5])
+        ax.set_ylim([0, 140])
+        ax.legend()
+        ax.grid(True)
+
+        return fig
